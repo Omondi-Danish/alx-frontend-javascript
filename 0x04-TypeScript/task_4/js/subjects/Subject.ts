0feaadb4ec52b3630447f9420c8e252b612b929b
Omondi Danish <omondidanish77@gmail.com>
2025-09-21 19:41:28 +0300

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,9 +2,9 @@
 
 namespace Subjects {
   export class Subject {
-    teacher: Subjects.Teacher;
+    teacher: Teacher;
 
-    setTeacher(teacher:Teacher): void {
+    setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
   }
